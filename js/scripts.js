78557b4c8b160ee183013feb59bae3ac2caf2a02
Ashley Kanas <kanasashley@gmail.com>
2024-10-23 15:55:01 -0400

    responsive hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log("Hi! Welcome to My Portfolio Site");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    var hamburger = document.querySelector('.nav_hamburger');
+    var closeIcon = document.querySelector('.nav_close');
+
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+      hamburger.style.display = 'none'; // Hide hamburger icon
+      closeIcon.style.display = 'inline'; // Show close icon
+    } else {
+      x.className = 'navtoggle';
+      hamburger.style.display = 'inline'; // Show hamburger icon
+      closeIcon.style.display = 'none'; // Hide close icon
+    }
+}
